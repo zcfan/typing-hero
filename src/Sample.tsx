2f4466f8ea513b318e85fa0c715de82d9b79294d
cfanzhang <cfanzhang@tencent.com>
2021-01-23 21:55:47 +0800

    随机选词

--- src/Sample.tsx
+++ src/Sample.tsx
@@ -1,5 +1,13 @@
+import words from './THUOCL_IT.json';
+
 export default function Sample() {
+  const word = randomWord()
   return (
-    <div>范例文本</div>
+    <div>{word[0]}</div>
   )
+}
+
+function randomWord() {
+  const index = Math.floor(Math.random() * words.length)
+  return words[index]
 }
